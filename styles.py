0d7e9bf4de1f888892657302ff02b99165d11d21
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2025-05-15 14:05:21 +0800

    First commit

---
+++ styles.py
@@ -0,0 +1,12 @@
+from tkinter.ttk import Style
+
+
+# Public variables for styling
+BUTTON_STYLE: str = "TButton"
+
+# Internal variables
+_BUTTON_STYLE: Style = Style()
+
+def configure_styles() -> None:
+    # _BUTTON_STYLE.configure("TButton", foreground="red", background="blue")
+    pass
